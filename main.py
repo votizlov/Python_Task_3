271e740b1dc4c675477ce54b5ef362afd7484d2e
Votizlov <SeriousFox@yandex.ru>
2020-03-19 22:12:28 +0300

    done

--- main.py
+++ main.py
@@ -1,63 +1,36 @@
-from section import Section
+from device import Device
 
-sections = []
-f = open("Test input/2.txt", "r")
+devices = []
+f = open("Test input/1.txt", "r")
 f1 = f.readlines()
 for x in f1:
     line = x.split()
-    sections.append(Section(int(line[0]), int(line[1])))
-
-
-def isOverlap(s):
-    for sec1 in s:
-        for sec2 in s:
-            if not (sec2 is sec1):
-                for sec3 in s:
-                    if not (sec3 is sec2 or sec3 is sec1):
-                        if (sec2.x1 <= sec1.x1 <= sec2.x2) or (sec2.x1 >= sec1.x2 >= sec2.x2):
-                            return 1
-                        if (sec2.x1 <= sec1.x2 <= sec2.x2) or (sec2.x1 >= sec1.x1 >= sec2.x2):
-                            return 1
-                        if (sec2.x1 <= sec3.x1 <= sec2.x2) or (sec2.x1 >= sec3.x2 >= sec2.x2):
-                            return 1
-                        if (sec2.x1 <= sec3.x2 <= sec2.x2) or (sec2.x1 >= sec3.x1 >= sec2.x2):
-                            return 1
-                        if (sec3.x1 <= sec1.x1 <= sec3.x2) or (sec3.x1 >= sec1.x2 >= sec3.x2):
-                            return 1
-                        if (sec3.x1 <= sec1.x2 <= sec3.x2) or (sec3.x1 >= sec1.x1 >= sec3.x2):
-                            return 1
-    return 0
-
-
-def lengthOfThree(s1, s2, s3):
-    return s1.len() + s2.len() + s3.len()
+    devices.append(Device(line[0], int(line[1]), int(line[2]), int(line[3])))
 
 
 def main():
-    maxLength = 0
-    indexes = []
-    i1 = 0
-    j1 = 0
-    k1 = 0
-    for i in sections:
-        i1 += 1
-        for j in sections:
-            j1 += 1
-            if not (j is i):
-                for k in sections:
-                    k1 += 1
-                    if not (k is j or k is i):
-                        if not isOverlap([i, j, k]) and lengthOfThree(i, j, k) > maxLength:
-                            maxLength = lengthOfThree(i, j, k)
-                            indexes = [i1, j1, k1]
-                            f = open("Output/1.txt", "w+")
-                            f.write("First " + str(i1))
-                            f.write(" Second " + str(j1))
-                            f.write(" Third " + str(k1))
-                k1 = 0
-        j1 = 0
-    print(maxLength)
-    print(indexes)
+    names = []
+    k = input("How many devices?")
+    m = input("Min memory")
+    r = input("Min rating")
+    price = 0
+    priceOfAll = 0
+    for i in range(int(k)):
+        price = devices[0].price
+        currentD = devices[0]
+        for d in devices:
+            if d.price < price and d.rate >= r and d.memo >= m:
+                currentD = d
+        names.append(currentD.name)
+        priceOfAll += price
+        devices.remove(currentD)
+
+    f = open("Output/1.txt", "w+")
+    f.write(" Price " + str(priceOfAll))
+    for name in names:
+        f.write(str(name))
+    print(price)
+    print(names)
 
 
 main()

c5d800cbe867b81e7ea67ee75776cb75b83c16d9
votizlov <35692043+votizlov@users.noreply.github.com>
2020-06-04 15:51:12 +0300

    done?

--- main.py
+++ main.py
@@ -11,39 +11,29 @@ def findCourceIndex(array, student):
 
 
 coursesWithStudents = []
+allStudents = []
 f = open("Test input/1.txt", "r")
 f1 = f.readlines()
 for x in f1:
     line = x.split()
     stud = Student(line[0], int(line[1]), line[2], int(line[3]))
+    allStudents.append(stud)
     if findCourceIndex(coursesWithStudents, stud) == -1:
         coursesWithStudents.append([stud])
     else:
         coursesWithStudents[findCourceIndex(coursesWithStudents, stud)].append(stud)
 
 
+def compUtil(s):
+    return s.avg
+
+
 def main():
     output = ""
     for cource in coursesWithStudents:
-        bestMales = [Student("", "", "male", 0)]
-        bestFemales = [Student("", "", "female", 0)]
-        for stud in cource:
-            if stud.sex == "male":
-                if stud.avg > bestMales[0].avg:
-                    bestMales.clear()
-                    bestMales.append(stud)
-                elif stud.avg == bestMales[0].avg:
-                    bestMales.append(stud)
-            if stud.sex == "female":
-                if stud.avg > bestFemales[0].avg:
-                    bestFemales.clear()
-                    bestFemales.append(stud)
-                elif stud.avg == bestFemales[0].avg:
-                    bestFemales.append(stud)
-        if bestMales[0].avg > 0 and bestFemales[0].avg > 0:
-            output = output + "Курс: " + str(cource[0].cource) + " " + random.choice(
-                bestFemales).name + " " + random.choice(
-                bestMales).name + "\n"
+        cource.sort(key=compUtil)
+
+
 
     f = open("Output/1.txt", "w+")
     f.write(output)
